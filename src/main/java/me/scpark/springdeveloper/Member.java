8b9da1f83beeee0b7f3d02fb1ef5a4aa749174f4
xiangyu204 <xiangyummd@gmail.com>
2026-03-12 12:19:40 +0900

    1

---
+++ src/main/java/me/scpark/springdeveloper/Member.java
@@ -0,0 +1,28 @@
+package me.scpark.springdeveloper;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+}
+package me.scpark.springdeveloper;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
